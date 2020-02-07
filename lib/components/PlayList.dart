eea0e4ceb721a0e562d1d28d841249055d831a20
zzzj <1152092752@qq.com>
2020-02-07 18:49:56 +0800

    完善播放列表组件

--- lib/components/PlayList.dart
+++ lib/components/PlayList.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:netease_music/modal/Song.dart';
+import 'package:netease_music/util/ColorsUtils.dart';
+import 'package:netease_music/util/ImageUtils.dart';
 
 import 'FlexibleDetailBar.dart';
 import 'PlayListHeader.dart';
@@ -61,17 +64,21 @@ class PlayList extends StatelessWidget {
             background: Stack(
               children: <Widget>[
                 httpBlurImage
-                    ? Image.network(
-                        this.blurImage,
+                    ? CachedNetworkImage(
+                        imageUrl: this.blurImage,
                         width: double.infinity,
                         height: double.infinity,
-                        fit: BoxFit.cover,
+                        fit: BoxFit.fill,
+                        placeholder: (context, url) {
+                          return ImageUtils.defaultBlurImage(
+                              BoxFit.fill, double.infinity, double.infinity);
+                        },
                       )
                     : Image.asset(
                         this.blurImage,
                         width: double.infinity,
                         height: double.infinity,
-                        fit: BoxFit.cover,
+                        fit: BoxFit.fill,
                       ),
 
                 /// 背景虚焦
@@ -81,26 +88,27 @@ class PlayList extends StatelessWidget {
                     sigmaX: this.sigmaX,
                   ),
                   child: Container(
-                    color: Colors.black38,
+                    color: Colors.transparent,
                     width: double.infinity,
                     height: double.infinity,
                   ),
                 ),
               ],
             ),
+
             /// 使用外部传入的content
             content: content,
           ),
-          expandedHeight: this.expandHeight ?? 120,
+          expandedHeight: this.expandHeight ?? 160,
           bottom: this.musicListHeader,
         ),
         SliverFixedExtentList(
           delegate: SliverChildBuilderDelegate(
             (context, index) {
-              return Container(
-                height: 50,
-                child: Center(
-                  child: Text((index + 1).toString()),
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: _Song(
+                  song: this.songList[index],
                 ),
               );
             },
@@ -110,5 +118,84 @@ class PlayList extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class _Song extends StatelessWidget {
+  final Song song;
+
+  _Song({Key key, this.song}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 40,
+        margin: EdgeInsets.symmetric(vertical: 2),
+        child: Row(
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.only(right: 20),
+                height: 40,
+                width: 60,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.network(
+                    song.coverUrl,
+                    fit: BoxFit.cover,
+                  ),
+                )),
+            Expanded(
+                flex: 1,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 26,
+                      width: double.infinity,
+                      padding: EdgeInsets.only(top: 8),
+                      child: Text(this.song.songName,
+                          maxLines: 1, overflow: TextOverflow.ellipsis),
+                    ),
+                    Container(
+                      height: 14,
+                      width: double.infinity,
+                      child: Text(
+                        "${this.song.singerName}   -   ${this.song.albumName}",
+                        style: TextStyle(color: Colors.grey, fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                )),
+            Container(
+              width: 30,
+              child: Center(
+                  child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        color: Colors.transparent,
+                        border:
+                            Border.all(color: ColorUtils.lightGrey(), width: 1),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: Colors.grey,
+                          size: 15,
+                        ),
+                      ))),
+            ),
+            Container(
+              width: 30,
+              child: Icon(
+                Icons.format_align_left,
+                color: Colors.grey,
+                size: 15,
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/components/PlayListHeader.dart
+++ lib/components/PlayListHeader.dart
@@ -2,8 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MusicListHeader extends StatelessWidget implements PreferredSizeWidget {
+  final int songListCount;
 
+  final VoidCallback onTap;
 
+  final bool showCount;
+
+  const MusicListHeader(
+      {Key key,
+      this.songListCount = 0,
+      @required this.onTap,
+      this.showCount = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +21,55 @@ class MusicListHeader extends StatelessWidget implements PreferredSizeWidget {
       borderRadius: BorderRadius.only(
           topLeft: Radius.circular(25), topRight: Radius.circular(25)),
       child: Container(
-        height: 20,
-        width: double.infinity,
-        color: Colors.white,
-        child: Text(
-          "12312312",
-          textAlign: TextAlign.center,
-        ),
-      ),
+          color: Colors.white,
+          height: 40,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Center(
+              child: Row(
+            children: <Widget>[
+              InkWell(
+                onTap: this.onTap,
+                child: Icon(
+                  Icons.play_circle_outline,
+                  size: 18,
+                ),
+              ),
+              Expanded(child: Builder(
+                builder: (BuildContext context) {
+                  if (showCount) {
+                    return Row(
+                      children: <Widget>[
+                        InkWell(
+                          child: Text(
+                            "  播放全部",
+                            style: TextStyle(fontSize: 13),
+                          ),
+                          onTap: this.onTap,
+                        ),
+                        Text(
+                          "共($songListCount)首",
+                          style: TextStyle(fontSize: 10, color: Colors.grey),
+                        )
+                      ],
+                    );
+                  } else {
+                    return InkWell(
+                      child: Text(
+                        "  播放全部",
+                        style: TextStyle(fontSize: 13),
+                      ),
+                      onTap: this.onTap,
+                    );
+                  }
+                },
+              )),
+              Icon(
+                Icons.menu,
+                size: 18,
+              ),
+              Text("  多选", style: TextStyle(fontSize: 13))
+            ],
+          ))),
     );
   }
 
